f03abd86afe03d282be58c0f851eac64ef6dde33
igorlogius <67047467+igorlogius@users.noreply.github.com>
2024-08-05 22:18:24 +0200

    inital

---
+++ README.md
@@ -0,0 +1,22 @@
+Quickly find text in all open and active tabs
+
+### [Click here to report a bug, make a suggestion or ask a question](https://github.com/igorlogius/igorlogius/issues/new/choose)
+
+<b>Short Demo Video:</b>
+
+tbd.
+
+<b>Usage/Features:</b>
+<ul>
+    <li>
+        Click the action button to add a site to the mute list and instantly mute them (the button will display (ON) for those sites.
+    </li>
+    <li>
+        Clicking the action button again will remove the site and unmute all muted tabs
+        (individual unmuting is not restircted, when gets overwriten, when sites are added or removed to the mute list)
+    </li>
+    <li>
+        Since the addon was made with Android in mind, the UI is as minimal as possible, and there is no overview of muted sites, 
+        The action button is the indication if a site is on the muted list
+    </li>
+</ul>
---
+++ background.js
@@ -0,0 +1,85 @@
+/* global browser */
+
+let mutedOrigins = new Set();
+
+async function getFromStorage(type, id, fallback) {
+  let tmp = await browser.storage.local.get(id);
+  return typeof tmp[id] === type ? tmp[id] : fallback;
+}
+
+async function setToStorage(id, value) {
+  let obj = {};
+  obj[id] = value;
+  return browser.storage.local.set(obj);
+}
+
+function onTabUpdated(tabId, changeInfo, tabInfo) {
+  if (changeInfo.url) {
+    if (mutedOrigins.has(new URL(changeInfo.url).origin)) {
+      browser.tabs.update(tabId, {
+        muted: true,
+      });
+      browser.browserAction.setBadgeText({ tabId, text: "ON" });
+    } else {
+      browser.tabs.update(tabId, {
+        muted: false,
+      });
+      browser.browserAction.setBadgeText({ tabId, text: "" });
+    }
+  }
+}
+
+browser.tabs.onUpdated.addListener(onTabUpdated, { properties: ["url"] });
+
+(async () => {
+  mutedOrigins = await getFromStorage("object", "mutedOrigins", new Set());
+})();
+
+browser.browserAction.onClicked.addListener(async (tab, info) => {
+  // get All Tabs that have the same origin
+  const url = new URL(tab.url);
+
+  let doOriginUnmute = false;
+  if (mutedOrigins.has(url.origin)) {
+    mutedOrigins.delete(url.origin);
+    // undo muted state for origin
+    doOriginUnmute = true;
+    browser.browserAction.setBadgeText({ tabId: tab.id, text: "" });
+  } else {
+    mutedOrigins.add(url.origin);
+    browser.browserAction.setBadgeText({ tabId: tab.id, text: "ON" });
+  }
+
+  setToStorage("mutedOrigins", mutedOrigins);
+
+  (await browser.tabs.query({})).forEach((t) => {
+    if (doOriginUnmute) {
+      if (t.url.startsWith(url.origin)) {
+        browser.tabs.update(t.id, {
+          muted: false,
+        });
+      }
+    } else if (mutedOrigins.has(new URL(t.url).origin)) {
+      browser.tabs.update(t.id, {
+        muted: true,
+      });
+    }
+  });
+
+  console.debug(await getFromStorage("object", "mutedOrigins", new Set()));
+});
+
+async function updateBadge(activeInfo) {
+  const atab = await browser.tabs.get(activeInfo.tabId);
+  if (mutedOrigins.has(new URL(atab.url).origin)) {
+    browser.browserAction.setBadgeText({ tabId: activeInfo.tabId, text: "ON" });
+  } else {
+    browser.browserAction.setBadgeText({ tabId: activeInfo.tabId, text: "" });
+  }
+}
+
+browser.tabs.onActivated.addListener(updateBadge);
+
+browser.browserAction.setBadgeBackgroundColor({
+  color: "green",
+});
